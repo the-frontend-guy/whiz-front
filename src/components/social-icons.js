dfbebdf33b4f85bbccc33152167f3b09e8a8a7d4
vikas pal <vikas.p@bewdigital.com>
2020-04-11 03:53:49 +0530

    social icons component structure linted

--- src/components/social-icons.js
+++ src/components/social-icons.js
@@ -1,20 +1,24 @@
-
 import React from "react"
 
 const SocialIcons = () => {
-  return(
+  return (
     <>
-    <div className="social-icon-container flex justify-end">
-      <a href="http:\\www.facebook.com" className="inline-block w-4 h-4"><img src="images/facebook.png" alt="facebook"/></a>
-      <a href="http:\\www.facebook.com" className="inline-block w-4 h-4 ml-8"><img src="images/facebook.png" alt="facebook"/></a>
-      <a href="http:\\www.facebook.com" className="inline-block w-4 h-4 ml-8"><img src="images/facebook.png" alt="facebook"/></a>
-      <a href="http:\\www.facebook.com" className="inline-block w-4 h-4 ml-8"><img src="images/facebook.png" alt="facebook"/></a>
-
-    </div>
+      <div className="social-icon-container flex justify-start md:justify-end">
+        <a href="http:\\www.facebook.com" className="inline-block w-4 h-4">
+          <img src="images/facebook.png" alt="facebook" />
+        </a>
+        <a href="http:\\www.facebook.com" className="inline-block w-4 h-4 ml-8">
+          <img src="images/facebook.png" alt="facebook" />
+        </a>
+        <a href="http:\\www.facebook.com" className="inline-block w-4 h-4 ml-8">
+          <img src="images/facebook.png" alt="facebook" />
+        </a>
+        <a href="http:\\www.facebook.com" className="inline-block w-4 h-4 ml-8">
+          <img src="images/facebook.png" alt="facebook" />
+        </a>
+      </div>
     </>
   )
 }
-
-
 
 export default SocialIcons
