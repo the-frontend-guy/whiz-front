8a5fc57793e0215430c69a852cd81a764199c6cf
vikas pal <vikas.p@bewdigital.com>
2020-05-04 20:26:52 +0530

    navigation scroll bar removed and js animation removed

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,29 +1,12 @@
 import { Link } from "gatsby"
-import React, {useEffect} from "react"
-import useMeasure from "react-use-measure"
+import React  from "react"
 
-let scrolled = 0;
-const Navigation = ({ active, data, windowEl }) => {
+const Navigation = ({ active, data }) => {
   const lists = [];
-  const [ref, bounds] = useMeasure()
   let navFixed = false;
   let transformNav = false;
 
-  if(windowEl.scrollY > (bounds.top + bounds.height)){
-    navFixed = true
-
-    if(scrolled > windowEl.scrollY){
-      transformNav = false;
-    } else {
-      transformNav = true;
-    }
-
-  } else {
-    navFixed = false;
-    transformNav = false;
-
-  }
-  scrolled = windowEl.scrollY;
+  
 
   data[0].strapiChildren.sort((a,b) => a.order - b.order).forEach(list => {
     lists.push(
@@ -33,7 +16,7 @@ const Navigation = ({ active, data, windowEl }) => {
     )
   })
   return(
-    <nav className={`p-4 md:p-5 bg-blue-100 top-0 transition ease-in-out duration-500 sticky  ${navFixed ? 'z-10' : ''} ${transformNav&&navFixed ? 'transform -translate-y-full': ''}`} ref={ref}>
+    <nav className={`p-4 md:p-5 bg-blue-100 top-0 transition ease-in-out duration-500 sticky z-10`}>
    <ul className="container inline-flex">
    {lists}
    </ul>
