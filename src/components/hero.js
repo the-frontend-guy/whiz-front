a22c5d28a8df4f3b2046f4ba4d6ed7ea71284ab6
vikas pal <vikas.p@bewdigital.com>
2020-05-04 20:45:28 +0530

    adjusted width of hero content

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,12 +2,12 @@ import React from "react"
 
 const Hero = ({ data }) => (
  <section className="bg-black hero">
-   <div className="content-container py-24 px-4 w-full md:w-9/12 lg:w-8/12 sm:px-16 lg:px-32">
+   <div className="content-container py-24 px-4 w-full md:w-7/12 sm:px-16 lg:px-32">
 
-   <h1 className="section-title text-white md:text-4xl lg:text-5xl leading-snug tracking-tight mb-8">
+   <h1 className="section-title text-white md:text-4xl lg:text-5xl xl:text-6xl  leading-snug tracking-tight mb-8">
      {data.heading}
    </h1>
-   <p className="text-white tracking-body mb-5">
+   <p className="text-white lg:text-lg tracking-body mb-5">
      {data.content}
    </p>
    </div>
