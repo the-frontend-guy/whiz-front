ec8421dedacdac99659c3e47c6918ce770f547db
vikas pal <vikas.p@bewdigital.com>
2020-05-04 21:07:47 +0530

    hero font increased

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,7 @@ const Hero = ({ data }) => (
  <section className="bg-black hero">
    <div className="content-container py-24 px-4 w-full md:w-7/12 sm:px-16 lg:px-32">
 
-   <h1 className="section-title text-white md:text-4xl lg:text-5xl xl:text-6xl  leading-snug tracking-tight mb-8">
+   <h1 className="section-title text-white md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl  leading-snug tracking-tight mb-8">
      {data.heading}
    </h1>
    <p className="text-white lg:text-lg tracking-body mb-5">
