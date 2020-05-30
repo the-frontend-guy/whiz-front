ea52114fe901175fc79a5e5a7c952df0509113bf
vikas pal <vikas.p@bewdigital.com>
2020-05-31 00:31:22 +0530

    contact us page completed

---
+++ src/pages/contact-us.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react"
+import ContactForm from "./../components/contact-form"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => {
+  return (
+    <Layout>
+      <section className="contact-us bg-black">
+        <div className="desc text-left md:text-center mb-8">
+          <h1 className="text-white section-title md:text-6xl px-4 md:px-0">Contact Us</h1>
+          <p className="text-white tracking-body mt-5 mb-1 px-4 md:px-0">
+            We are always happy to talk with you
+          </p>
+          <p className="text-white tracking-body mb-5  px-4 md:px-0">
+            Feel free to contact us in any suitable way
+          </p>
+        </div>
+
+        <div className="content-wrapper flex  flex-col md:items-center md:flex-row">
+          <div className="px-4 w-full md:px-12 md:w-4/12">
+            <h2 className="text-white section-title text-4xl">Let's talk</h2>
+            <p className="text-white tracking-body mt-4">
+              Let's discuss your project!
+            </p>
+          </div>
+          <div className="px-4 w-full md:w-8/12 md:px-12">
+            <ContactForm />
+          </div>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default ContactPage
